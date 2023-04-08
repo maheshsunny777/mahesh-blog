75db6737ea5e47baed63cb758e599594a5f49de3
Mahesh Karnakoti <maheshsunny777@gmail.com>
2023-04-08 14:54:35 -0400

    updating main page text

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Mahesh Karnakoti';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'A blog to document DevOps and Security Learnings';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'All rights reserved 2023.';
 
   return {
     name,
